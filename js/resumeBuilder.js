d09a3e768cbce656d680fbf7ad979f3d53cb3ace
lnara002 <lakshman_kumar.narayanan@nokia.com>
2017-03-27 19:14:52 -0700

    practise strings

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%","Lakshman Kumar Narayanan");
+var formattedRole = HTMLheaderRole.replace("%data%","Software Engineer");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
